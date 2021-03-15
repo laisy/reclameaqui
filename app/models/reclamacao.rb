a26e09910d2e7896747978adccbaa7d40f9c46fd
laisy <laisyferreira2011@gmail.com>
2021-03-15 11:05:10 -0300

    Alterando idioma das mensagens de validaçao de reclamaçao

--- app/models/reclamacao.rb
+++ app/models/reclamacao.rb
@@ -1,7 +1,7 @@
 class Reclamacao < ApplicationRecord
   belongs_to :usuario
-  validates :titulo, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, maximum:20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
-  validates :cat_problema, presence: {message: "campo obrigatório"}, length: {minimum: 5, maximum:20, too_long: "%{count} characters is the maximum allowed" }
-  validates :texto, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, maximum:500, too_long: "%{count} characters is the maximum allowed" }
-  validates :nome_empresa, presence: {message: "campo obrigatório"},  length: {minimum: 3, maximum:20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
+  validates :titulo, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, too_short: "%{count} caracteres é o minimo permitido ", maximum:20, too_long: "%{count} caracteres é o máximo permitido " }, uniqueness: true
+  validates :cat_problema, presence: {message: "campo obrigatório"}, length: {minimum: 5 , too_short: "%{count} caracteres é o minimo permitido ", maximum:20, too_long: "%{count} caracteres é o máximo permitido " }
+  validates :texto, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, too_short: "%{count} caracteres é o minimo permitido ", maximum:500, too_long: "%{count} caracteres é o máximo permitido " }
+  validates :nome_empresa, presence: {message: "campo obrigatório"},  length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum:20, too_long: "%{count} caracteres é o máximo permitido " }, uniqueness: true
 end
