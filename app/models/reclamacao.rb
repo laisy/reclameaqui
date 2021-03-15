71596174c127db1d78f7b8a5f697bd5ac2a998c4
laisy <laisyferreira2011@gmail.com>
2021-03-15 10:41:05 -0300

    Adicionando tamanho maximo e minimo para categoria da reclamaçao

--- app/models/reclamacao.rb
+++ app/models/reclamacao.rb
@@ -1,7 +1,7 @@
 class Reclamacao < ApplicationRecord
   belongs_to :usuario
   validates :titulo, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, maximum:20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
-  validates :cat_problema, presence: {message: "campo obrigatório"}
+  validates :cat_problema, presence: {message: "campo obrigatório"}, length: {minimum: 5, maximum:20, too_long: "%{count} characters is the maximum allowed" }
   validates :texto, presence: {message: "campo obrigatório"}, format: {with: /[A-Za-z0-9]+/}, length: {minimum: 5, maximum:500, too_long: "%{count} characters is the maximum allowed" }
   validates :nome_empresa, presence: {message: "campo obrigatório"},  length: {minimum: 3, maximum:20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
 end
