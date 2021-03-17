a8191b94b6969b331dc4e603adc1dbccdf11c8ae
laisy <laisyferreira2011@gmail.com>
2021-03-16 21:22:29 -0300

    Teste de Model editando usuario

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -5,4 +5,10 @@ class UsuarioTest < ActiveSupport::TestCase
     usuario = Usuario.new login:'joaoo', email: 'joao@email.com', senha:'123456'
     assert usuario.save
    end
+  test "editando usuario" do
+    usuario = Usuario.new login:'joaoo', email: 'joao@email.com', senha:'123456'
+    usuario.save
+    assert usuario.update login:'joaooo', email: 'joao@email.com', senha:'123456'
+   end
+  
 end
