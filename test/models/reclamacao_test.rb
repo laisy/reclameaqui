cabd45bdd32b05f6506c9bc3e874c01448d9b89e
laisy <laisyferreira2011@gmail.com>
2021-03-16 21:29:30 -0300

    Teste de Model editando reclamacao com titulo muito grande

--- test/models/reclamacao_test.rb
+++ test/models/reclamacao_test.rb
@@ -19,4 +19,10 @@ class ReclamacaoTest < ActiveSupport::TestCase
     reclamacao = Reclamacao.new titulo:'PC quebrado', texto:'', cat_problema:'Informatica', nome_empresa:'Empresa1', usuario_id: usuario.id
     assert_not reclamacao.save
   end
+  test "editando reclamacao com titulo muito grande" do
+    usuario = Usuario.new login:'joaoo', email: 'joao@email.com', senha:'123456'
+    usuario.save
+    reclamacao = Reclamacao.new titulo:'PC quebrado', texto:'PC comprado defeituoso', cat_problema:'Informatica', nome_empresa:'Empresa1', usuario_id: usuario.id
+    assert_not reclamacao.update titulo:'PC defeituoso e sem pegar', texto:'PC comprado defeituoso', cat_problema:'Informatica', nome_empresa:'Empresa1', usuario_id: usuario.id
+  end
 end
